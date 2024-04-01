40f5666ec8c3f83b11964f0713e0ecdb846b5fc6
Jorge Lucas Cavalcante <jorge.lsc@ufma.br>
2024-04-01 13:56:38 -0300

    Iniciando Curso 3

---
+++ src/main/java/br/com/ufma/apimed/model/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,24 @@
+package br.com.ufma.apimed.model.paciente;
+
+import br.com.ufma.apimed.model.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")
+        String cpf,
+
+        @NotNull @Valid DadosEndereco endereco) {
+}
---
+++ src/main/java/br/com/ufma/apimed/model/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,11 @@
+package br.com.ufma.apimed.model.paciente;
+
+
+import br.com.ufma.apimed.model.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+    }
+}
